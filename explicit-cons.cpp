25c12f596d6791c73781fd7d900b1c9e0691fa8e
Larry Ruane <larryruane@gmail.com>
2023-02-06 20:39:13 -0700

    fix comment typo

--- explicit-cons.cpp
+++ explicit-cons.cpp
@@ -5,24 +5,19 @@ class Foo
 {
 public:
     // single parameter constructor, can be used as an implicit conversion
-    explicit Foo (int foo) : m_foo (foo) 
-    {
-    }
-
+    explicit Foo (int foo) : m_foo (foo) {}
     int GetFoo() { return m_foo; }
-
 private:
     int m_foo;
 };
 
-void DoBar(Foo foo)
-{
+void DoBar(Foo foo) {
     int i = foo.GetFoo();
 }
 
 int main()
 {
-    // this doesn't compile because off the explicit above
+    // this doesn't compile because of the 'explicit' above
     //DoBar(42);
 
     // this does work (with the explicit):
